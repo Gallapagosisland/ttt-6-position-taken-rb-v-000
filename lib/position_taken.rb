af5fa5c2b46f487fde3f36cce4ea9a9d288b89ff
Connor Gallopo <connor.gallopo@me.com>
2017-10-14 13:59:26 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@ def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     # you got hung up here because you didnt put board[index] for each check. you only checked for " ". not "" or nil
     return false
-  elsif board[index] == "X" || board[index] "O"
+  elsif board[index] == "X" || board[index] == "O"
     return true
 
   end
