50b7b0fa99506889690504adc1d01930e4ac7009
Connor Gallopo <connor.gallopo@me.com>
2017-10-14 13:43:20 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,8 +2,8 @@
 def position_taken?(board, index)
   if board[index] == " " || "" || nil
     return false
-  elsif board[index] == "X" || "O"
-    return false
+  else board[index] == "X" || "O"
+    return true
 
   end
 
